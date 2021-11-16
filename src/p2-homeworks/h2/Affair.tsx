91cc33bab61013780b2c11ba34a511aef644b75d
Nick <74413828+ns0ff@users.noreply.github.com>
2021-11-16 18:41:19 +0300

    homework 2 is completed

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import styles from './Affairs.module.css'
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType // any fixed
+    deleteAffairCallback: (id: number) => void // any fixed
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = (id: number) => {
+        props.deleteAffairCallback(props.affair._id)
+    }// fixed
 
     return (
         <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+            <div className={styles.affairContainer}>
+                <div className={styles.affairName}> {props.affair.name} </div>
+                <div className={styles.affairPriority}>{props.affair.priority}</div>
+                <button onClick={() => deleteCallback(props.affair._id)}>X</button>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
 import Affairs from './Affairs'
+import style from "../../p1-main/m1-ui/u1-app/homework.module.css";
 
 // types
-export type AffairPriorityType = any // need to fix any
-export type AffairType = any // need to fix any
+export type AffairPriorityType = 'high' | 'middle' | 'low' // any fixed
+export type AffairType = {
+    _id: number
+    name: string
+    priority: AffairPriorityType
+    } // any fixed
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: Array<AffairType> = [ // any fixed
     {_id: 1, name: 'React', priority: 'high'},
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -16,37 +21,38 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // any fixed
     if (filter === 'all') return affairs
-    else return // need to fix
+    if (filter === 'high') return affairs.filter(a => a.priority === 'high')
+    if (filter === 'middle') return affairs.filter(a => a.priority === 'middle')
+    if (filter === 'low') return affairs.filter(a => a.priority === 'low')
+    else return affairs // fixed
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-    return // need to fix
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => { // any fixed
+    return affairs.filter(a => a._id !== _id)// fixed
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs) // any fixed
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id)) // any fixed
 
     return (
-        <div>
-            <hr/>
-            homeworks 2
+        <div className={style.homeworkContainer}>
+            <span className={style.homeworkTitle}>Homework 2</span>
 
-            {/*should work (должно работать)*/}
             <Affairs
-                data={filteredAffairs}
+                filteredAffairs={filteredAffairs}
                 setFilter={setFilter}
                 deleteAffairCallback={deleteAffairCallback}
             />
 
-            <hr/>
+            {/*<hr/>*/}
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeAffairs/>*/}
-            <hr/>
+            {/*<hr/>*/}
         </div>
     )
 }
