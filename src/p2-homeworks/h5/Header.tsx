3177cde2e54d8eaccdfbe83a359eec9bdfa71f2c
Nick <74413828+ns0ff@users.noreply.github.com>
2021-12-02 23:50:15 +0300

    Update Header.tsx

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import {NavLink} from "react-router-dom"
+import {NavLink, useMatch} from "react-router-dom"
 import s from './Header.module.css'
 
 function Header() {
+    let match = useMatch('/PRE_JUNIOR')
+
     return (
         <div className={s.headerComp}>
             <div className={s.container}>
                 <div className={s.navLinks}>
-                    <NavLink to={'/'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Main Page </NavLink>
+                    <NavLink to={'/'} className={(navLink) => (match ? s.active : s.navLink)}> Main Page </NavLink>
                     <NavLink to={'/hw1'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 1</NavLink>
                     <NavLink to={'/hw2'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 2</NavLink>
                     <NavLink to={'/hw3'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 3</NavLink>
