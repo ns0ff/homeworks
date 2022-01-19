e1a8efdfff72ec0a83c8bb873e5773a7b7177071
Nick <74413828+ns0ff@users.noreply.github.com>
2022-01-19 18:39:16 +0300

    homework 5 refactoring

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import {NavLink, useMatch} from "react-router-dom"
+import {NavLink} from "react-router-dom"
 import s from './Header.module.css'
+import {PATH} from "./Routes";
 
 function Header() {
-    let match = useMatch('/PRE_JUNIOR')
+    // let match = useMatch('/PRE_JUNIOR')
 
     return (
         <div className={s.headerComp}>
             <div className={s.container}>
                 <div className={s.navLinks}>
-                    <NavLink to={'/'} className={(navLink) => (match ? s.active : s.navLink)}> Main Page </NavLink>
-                    <NavLink to={'/hw1'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 1</NavLink>
-                    <NavLink to={'/hw2'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 2</NavLink>
-                    <NavLink to={'/hw3'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 3</NavLink>
-                    <NavLink to={'/hw4'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 4</NavLink>
+                    {/*<NavLink to={'/'} className={(navLink) => (match ? s.active : s.navLink)}> Main Page </NavLink>*/}
+                    {/*<NavLink to={'/hw1'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 1</NavLink>*/}
+                    {/*<NavLink to={'/hw2'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 2</NavLink>*/}
+                    {/*<NavLink to={'/hw3'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 3</NavLink>*/}
+                    {/*<NavLink to={'/hw4'} className={(navLink) => (navLink.isActive ? s.active : s.navLink)}> Homework 4</NavLink>*/}
+                    <NavLink to={PATH.PRE_JUNIOR} className={(navLink) => navLink.isActive ? s.active : s.navLink}>Pre-Junior</NavLink>
+                    <NavLink to={PATH.JUNIOR} className={(navLink) => navLink.isActive ? s.active : s.navLink}>Junior</NavLink>
+                    <NavLink to={PATH.JUNIOR_PLUS} className={(navLink) => navLink.isActive ? s.active : s.navLink}>Junior Plus</NavLink>
                 </div>
             </div>
         </div>
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import {Routes, Route, Navigate} from "react-router-dom";
-import HW1 from "../h1/HW1";
-import HW2 from "../h2/HW2";
-import HW3 from "../h3/HW3";
-import HW4 from "../h4/HW4";
+// import HW1 from "../h1/HW1";
+// import HW2 from "../h2/HW2";
+// import HW3 from "../h3/HW3";
+// import HW4 from "../h4/HW4";
 import Error404 from "./pages/Error404";
 import PreJunior from "./pages/PreJunior";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/JuniorPlus";
 
 export const PATH = {
-    PRE_JUNIOR: "/PRE_JUNIOR",
-    HW1: "/hw1",
-    HW2: "/hw2",
-    HW3: "/hw3",
-    HW4: "/hw4",
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus'
 }
 
 function RoutesTo() {
@@ -21,12 +21,14 @@ function RoutesTo() {
             <Routes>
                 {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
-                <Route path="/" element={<Navigate replace to="/PRE_JUNIOR" />} />
-                <Route path="/PRE_JUNIOR"  element={<PreJunior/>}/>
-                <Route path="/hw1"  element={<HW1/>}/>
-                <Route path="/hw2"  element={<HW2/>}/>
-                <Route path="/hw3"  element={<HW3/>}/>
-                <Route path="/hw4"  element={<HW4/>}/>
+                <Route path="/" element={<Navigate replace to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                {/*<Route path="/hw1"  element={<HW1/>}/>*/}
+                {/*<Route path="/hw2"  element={<HW2/>}/>*/}
+                {/*<Route path="/hw3"  element={<HW3/>}/>*/}
+                {/*<Route path="/hw4"  element={<HW4/>}/>*/}
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
                 <Route element={<Error404/>}/>
             </Routes>
